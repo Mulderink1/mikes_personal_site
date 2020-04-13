047504fe2a81862f62593b8feb727a72199c3316
Mike Mulderink <mulderink.code@gmail.com>
2020-04-13 16:56:55 -0500

    Finished Building UP page

--- server/server.js
+++ server/server.js
@@ -5,8 +5,12 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 app.use("/", express.static(path.resolve(__dirname, "../client/assets")));
+app.use("/melissa-mike", express.static(path.resolve(__dirname, "../client/up-assets")));
 app.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname, "../client/views/index.html"));
+});
+app.get("/melissa-mike", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../client/views/upTrip.html"));
 });
 app.listen(PORT, function () {
     var newLocal = "listing on http://localhost:" + PORT + "/";
