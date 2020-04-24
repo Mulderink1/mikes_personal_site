5dc8ca869c45ba46cddaf0f0dd145eca784dc7ad
Mike Mulderink <mulderink.code@gmail.com>
2020-04-24 15:53:16 -0500

    Fixed Modal Sizing

--- client/up-assets/js/index.js
+++ client/up-assets/js/index.js
@@ -5,13 +5,14 @@ var pictureModal = function (thisObject) {
             text: thisObject.childNodes[3].childNodes[1].innerHTML
         };
         var backgoundDiv_1 = document.createElement("div");
-        backgoundDiv_1.setAttribute("class", "background-div");
         var modalDiv = document.createElement("div");
+        var imageDiv = document.createElement("div");
         var imageTag = document.createElement("img");
         imageTag.setAttribute("src", imageData.src);
         var paragraphTag = document.createElement("p");
         paragraphTag.innerHTML = imageData.text;
-        modalDiv.appendChild(imageTag);
+        imageDiv.appendChild(imageTag);
+        modalDiv.appendChild(imageDiv);
         modalDiv.appendChild(paragraphTag);
         backgoundDiv_1.appendChild(modalDiv);
         document.getElementsByTagName("main")[0].appendChild(backgoundDiv_1);
